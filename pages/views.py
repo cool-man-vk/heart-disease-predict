7a3be2df96c49220202bb295e83e6b96a5a30c4e
Vignesh kumar <rrvigneshkumar2002@gmail.com>
2022-08-06 21:26:56 +0530

    models loaded successfully

--- pages/views.py
+++ pages/views.py
@@ -15,7 +15,7 @@ def analyseResult(request):
         age = request.POST.get('age')
         sex = request.POST.get('sex')
         cpt = request.POST.get('cpt')
-        trest_bps = request.POST.get('trestbps')
+        trest_bps = request.POST.get('trest_bps')
         chol = request.POST.get('chol')
         fbs = request.POST.get('fbs')
         rest_ecg = request.POST.get('rest-ecg')
@@ -27,12 +27,16 @@ def analyseResult(request):
         th_def_type = request.POST.get('thal')
 
         
+           
+
+
         heart_data = pd.read_csv('heart_disease_data.csv')
         X = heart_data.drop(columns='target',axis=1)
+        print(X)
         y = heart_data['target']
         X_train , X_test , y_train , y_test = train_test_split(X,y,test_size=0.2 , stratify=y , random_state=2)
-        dataset_details = [age , sex , cpt , trest_bps , chol , fbs, rest_ecg , thalach , exang , old_peak , slope , ca , th_def_type ]
-
+        dataset_details = [int(age) , int(sex) , int(cpt) , int(trest_bps) , int(chol) , int(fbs), int(rest_ecg) , int(thalach) , int(exang) , float(old_peak) , int(slope) , int(ca) , int(th_def_type) ]
+        print(dataset_details)
         model = LogisticRegression()
         model.fit(X_train , y_train)
 
@@ -46,16 +50,14 @@ def analyseResult(request):
         input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
 
         prediction = model.predict(input_data_reshaped)
-        predict_disease(prediction[0])
-
-        def predict_disease(pred):
-            if(pred == 0):
+        if(prediction[0] == 0):
                 st = 'The person doesn\'t have heart disease'
                 return render(request,'pages/results.html',{'has_disease':st})
-            else:
+        else:
                 st = 'The person has a heart disease'
                 return render(request,'pages/results.html',{'has_disease':st})
 
+        
     
     return render(request,'pages/analyse.html',{})
 
